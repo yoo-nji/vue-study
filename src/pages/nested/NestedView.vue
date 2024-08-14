9146c2135b7bf90d376e90d1b7a3e3b1df20217b
yoonji <kyjkbw@gmail.com>
2024-08-14 11:54:58 +0900

    fix: Nested 페이지 이동 시 히스토리에 쌓지 않도록 replace 옵션 적용

--- src/pages/nested/NestedView.vue
+++ src/pages/nested/NestedView.vue
@@ -2,12 +2,20 @@
   <div>
     <ul class="nav nav-pills">
       <li class="nav-item">
-        <RouterLink class="nav-link" active-class="active" to="/nested/one">
+        <RouterLink
+          class="nav-link"
+          active-class="active"
+          :to="{ name: 'NestedOne', replace: true }"
+        >
           Nested One
         </RouterLink>
       </li>
       <li class="nav-item">
-        <RouterLink class="nav-link" active-class="active" to="/nested/two">
+        <RouterLink
+          class="nav-link"
+          active-class="active"
+          :to="{ name: 'NestedTwo', replace: true }"
+        >
           Nested Two
         </RouterLink>
       </li>
